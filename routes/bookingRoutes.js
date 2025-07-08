38c8e5dae37e4c23b1d08f36a793c3d4cd9226ef
Shreehari Nair <nairshreehari805@gmail.com>
2025-07-08 10:03:45 +0530

    Initial commit

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bookingController = require('./../controllers/bookingController');
+const authController = require('./../controllers/authController.js');
+
+// POST /tour/234fad4/reviews
+// POST /reviews
+
+const router = express.Router({ mergeParams: true });
+
+router.use(authController.protect);
+
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
+
+module.exports = router;
